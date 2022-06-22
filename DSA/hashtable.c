b5fc25d8807fb49c013ad5e740ef4af296b40810
ken ken <ken20020209@gmail.com>
2022-06-22 15:05:09 +0800

    test

--- DSA/hashtable.c
+++ DSA/hashtable.c
@@ -1,53 +1,59 @@
 #include "../header/leetcode.h"
 #define hash_table_size 500
-struct hash_node
+typedef struct HashNode
 {
     int val;
-    int len;
-    int address;
-    //struct hash_node*next;
-};
-struct hash_map{
+    struct HashNode *pre;
+    struct HashNode*next;
+}HashNode;
+typedef struct HashTableHead
+{
+    int length;
+    struct HashNode *node;
+}HashTableHead;
+typedef struct HashTable
+{
     int size;
-    struct hash_node*data;
-};
-int hash_key(int key)
+    int loadfactor;
+    int capacity;
+    struct HashTableHead*data;
+}HashTable;
+int BKDhash(char* str)
 {
-    //printf("key=%d\n",key);
-    return abs(key%hash_table_size);
+    int seed=31,hash=0;
+    for (int i = 0; str[i]; i++)hash=hash*seed+str[i];
+    return hash&0x7fffffff;
+    
 }
-struct hash_map *hash_map_create()
+HashTable* HashTableCreate(int capacity)
 {
-    //printf("%d\n",sizeof(struct hash_map*));
-    struct hash_map* table=malloc(sizeof(struct hash_map));
-    table->size=hash_table_size;
-    //printf("%d\n",table->size);
-    table->data=calloc(table->size,sizeof(struct hash_node));
-    return table;
+    HashTable* ht=(HashTable*)malloc(sizeof(HashTable));
+    if(ht==NULL)return NULL;
+    ht->size=0;
+    ht->capacity=capacity;
+    ht->loadfactor=0;
+    ht->data=(HashTableHead*)malloc(capacity*sizeof(HashTableHead));
+    if(ht->data==NULL)
+    {
+        free(ht);
+        return NULL;
+    }
+    return ht;
+    
 }
-void hash_map_put(struct hash_map *table,int val,int address)
-{
-    int key=hash_key(val);
-    table->data[key].val=val;
-    table->data[key].len++;
-    table->data[key].address=address;
-}
-int hash_map_get_address(struct hash_map *table,int val)
-{
-    int key=hash_key(val);
-    if(table->data[key].len==0)return -1;
-    return table->data[key].address;
-}
-int hash_map_serch(struct hash_map *table,int val)
-{
-    int key=hash_key(val);
-    //printf("key=%d\n",key);
-    if(table->data[key].len==0)return -1;
-    if(table->data[key].val==val)return 1;
-    return 0;
-}
-void hash_map_free(struct hash_map *table)
+void HashTableFree(HashTable* table)
 {
     free(table->data);
     free(table);
+}
+int main()
+{
+    char str[2];
+    str[1]=0;
+    for (int i = 0; i < 256; i++)
+    {
+        str[0]=i;
+        printf("%s:%x\n",str,BKDhash(str));
+    }
+    
 }
--- DSA/vector.c
+++ DSA/vector.c
@@ -1,5 +1,14 @@
 typedef struct
 {
     int capacity;
+    int size;
     int *data;
 }MyVector;
+MyVector* vector_create(int size)
+{
+    MyVector* v= (MyVector *)malloc(sizeof(MyVector));
+    v->data= (int*)malloc(sizeof(int)*size);
+    v->size=0;
+    v->capacity=size;
+    return v;
+}
