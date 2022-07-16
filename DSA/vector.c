1027d85e013c3cf7137bb61fc554e36d592eaba6
ken ken <ken20020209@gmail.com>
2022-07-16 15:46:31 +0800

    vecotr test extend

--- DSA/vector.c
+++ DSA/vector.c
@@ -21,6 +21,15 @@ void vector_extend(Vector *v)
 void vecotr_add(Vector *v, void *data)
 {
     if (v->size == v->capacity)
-        vector_extend(v, data);
+        vector_extend(v);
     v->data[v->size++] = data;
+}
+int main()
+{
+    Vector *v = vector_create(2);
+    for (int i = 0; i < 100; i++)
+    {
+        vecotr_add(v, (void *)i);
+    }
+    return 0;
 }
